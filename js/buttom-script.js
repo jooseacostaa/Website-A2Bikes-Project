3621abdb63a9d6bee311b16f1c68fabc824af2dc
jooseacostaa <joseacostanaranjo@hotmail.com>
2025-12-08 02:40:55 +0000

    Format update

--- js/buttom-script.js
+++ js/buttom-script.js
@@ -1,20 +1,23 @@
 const SCROLL_TO_TOP_BTN = document.getElementById("scroll-to-top-btn");
 
 function toggleScrollButtonVisibility() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        SCROLL_TO_TOP_BTN.style.display = "block";
-    } else {
-        SCROLL_TO_TOP_BTN.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    SCROLL_TO_TOP_BTN.style.display = "block";
+  } else {
+    SCROLL_TO_TOP_BTN.style.display = "none";
+  }
 }
 
 window.onscroll = toggleScrollButtonVisibility;
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 SCROLL_TO_TOP_BTN.addEventListener("click", scrollToTop);
